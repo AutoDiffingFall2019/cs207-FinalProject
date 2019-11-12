fa830b9a50c0f5437773b3b50d3f0b9fad475924
Shuvom Sadhuka <ssadhuka@college.harvard.edu>
2019-11-12 13:20:17 -0500

    Add test.py

--- code/AutoDiff/test.py
+++ code/AutoDiff/test.py
@@ -5,9 +5,9 @@ from ElementaryFunctions import *
 
 a = DualNumber(5)
 
-b = Cos(a)
-c = Sqrt(Sin(b)) + b
-d = c**5
+b = Exp(a)
+c = Cos(Sqrt(b)) + b**2
+d = Log(c)
 
 print(c.val, c.der)
 print(d.val, d.der)
