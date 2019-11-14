9c85a7cd9589007e3a15f160363f4e0e377fb7db
edwin-chng <chng_weimingedwin@g.harvard.edu>
2019-11-14 14:06:19 -0500

    Update ElementaryFunctions

--- code/AutoDiff/ElementaryFunctions.py
+++ code/AutoDiff/ElementaryFunctions.py
@@ -3,79 +3,111 @@ import numpy as np
 
 class Sin(DualNumber):
     def __init__(self, x):
-        data_type_check(x)
-        self._val = np.sin(x._val)
-        self._der = np.cos(x._val)*x._der
- 
+	data_type_check(x)
+	if data_type_check(x) == 0:
+            self._val = np.sin(x._val)
+            self._der = np.cos(x._val)*x._der
+        else:
+            self._val = np.sin(x)
+            self._der = 0
     
 class Tan(DualNumber):
     def __init__(self, x):
         data_type_check(x)
-        self._val = np.tan(x._val)
-        self._der = (1+np.tan(x._val)*np.tan(x._val))*x._der
-        
+	if data_type_check(x) == 0:
+            self._val = np.tan(x._val)
+            self._der = (1+np.tan(x._val)*np.tan(x._val))*x._der
+        else:
+            self._val = np.tan(x)
+            self._der = 0	
 
 class Cos(DualNumber):
-    def __init__(self, x):
-        print(x._val)
-        print(x._der)
-        data_type_check(x)
-        self._val = np.cos(x._val)
-        self._der = -1*np.sin(x._val)*x._der
-
+    def __init__(self,x):
+	data_type_check(x)
+        if data_type_check(x) == 0:
+            self._val = np.cos(x._val)
+            self._der = -1*np.sin(x._val)*x._der
+        else:
+            self._val = np.cos(x)
+            self._der = 0
 
 class Exp(DualNumber):
     def __init__(self, x):
         data_type_check(x)
-        self._val = np.exp(x._val)
-        self._der = np.exp(x._val)*x._der
-
+        if data_type_check(x) == 0:
+            self._val = np.exp(x._val)
+            self._der = np.exp(x._val)*x._der
+        else:
+            self._val = np.exp(x)
+            self._der = 0
 
 class Power(DualNumber):
     def __init__(self, x, n):
         data_type_check(x)
-        self._val = x._val**n
-        self._der = n*(x._val**(n-1))*x._der
-        
+        if data_type_check(x) == 0:
+            self._val = x._val**n
+            self._der = n*(x._val**(n-1))*x._der
+        else:
+            self._val = x**n
+            self._der = 0
 
 class Log(DualNumber):
     def __init__(self, x):
-        data_type_check(x)
-        self._val = np.log(x._val)
-        self._der = (1/x._val)*x._der
-        
+	data_type_check(x)
+        if data_type_check(x) == 0:
+            self._val = np.log(x._val)
+            self._der = (1/x._val)*x._der
+        else:
+            self._val = np.log(x)
+            self._der = 0 
         
 class ArcSin(DualNumber):
     def __init__(self, x):
         data_type_check(x)
-        self._val = np.arcsin(x._val)
-        self._der = 1/np.sqrt(1-x._val**2) * x._der
-
+        if data_type_check(x) == 0:
+            self._val = np.arcsin(x._val)
+            self._der = 1/np.sqrt(1-x._val**2) * x._der
+        else:
+            self._val = np.arcsin(x)
+            self._der = 0
 
 class ArcCos(DualNumber):
     def __init__(self, x):
         data_type_check(x)
-        self._val = np.arccos(x._val)
-        self._der = -1/np.sqrt(1-x._val**2) * x._der
-            
+        if data_type_check(x) == 0:
+            self._val = np.arccos(x._val)
+            self._der = -1/np.sqrt(1-x._val**2) * x._der
+        else:
+            self._val = np.arccos(x)
+            self._der = 0            
             
 class ArcTan(DualNumber):
     def __init__(self, x):
         data_type_check(x)
-        self._val = np.arctan(x._val)
-        self._der = 1/np.sqrt(1+x._val**2) * x._der
-
+        if data_type_check(x) == 0:
+            self._val = np.arctan(x._val)
+            self._der = 1/np.sqrt(1+x._val**2) * x._der
+        else:
+            self._val = np.arctan(x)
+            self._der = 0
 
 class Sqrt(DualNumber):
     def __init__(self, x):
         data_type_check(x)
-        self._val = np.sqrt(x._val)
-        self._der = 1/(2*np.sqrt(x._val)) * x._der
-        
+        if data_type_check(x) == 0:
+            self._val = np.sqrt(x._val)
+            self._der = 1/(2*np.sqrt(x._val)) * x._der
+        else:
+            self._val = np.sqrt(x)
+            self._der = 0        
         
 def data_type_check(x):
    try:
        float(x._val)+float(x._der)
-   except ValueError:
-       print("Input has to have attributes x.val and x.der")
-    
+       return 0  # returns 0 if x is DualNumber
+   except AttributeError:
+       try:
+           float(x)
+           return 1 # returns 1 if x is real
+       except:
+           raise AttributeError('Input must be dual number or real number!')    
