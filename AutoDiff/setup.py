d71ae266b3302fd7cfe1154422721757d89ffbd8
ssadhuka <43124999+ssadhuka@users.noreply.github.com>
2019-11-19 21:21:46 -0500

    Update setup.py

--- AutoDiff/setup.py
+++ AutoDiff/setup.py
@@ -3,9 +3,16 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("requirements.txt", "r") as rt:
+    install_reqs = rt.read().splitlines() 
+
+
 setuptools.setup(
     name="autodiffing",
-    version="0.0.1",
+    version="0.0.5",
+    
+    install_requires=install_reqs,
+    
     author="Shuvom, Sijie, Silin, Edwin",
     author_email="ssadhuka@college.harvard.edu, sijiesun@g.harvard.edu, szou@g.harvard.edu, chng_weimingedwin@g.harvard.edu",
     description="A package for Automatic Differentiation",
